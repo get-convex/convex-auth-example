fc7216a2e0cd5f934d83f67e8a690d7995f63846
sanjayr-12 <sanjayr.bit22@gmail.com>
2024-09-26 12:57:53 +0530

    Implement message deletion on double-click

--- src/Chat/Message.tsx
+++ src/Chat/Message.tsx
@@ -1,18 +1,39 @@
 import { cn } from "@/lib/utils";
 import { ReactNode } from "react";
 import { Id } from "../../convex/_generated/dataModel";
+import { useMutation } from "convex/react";
+import { api } from "../../convex/_generated/api";
+import { error } from "console";
 
 export function Message({
   authorName,
   author,
   viewer,
   children,
+  id,
 }: {
   authorName: string;
   author: Id<"users">;
   viewer: Id<"users">;
   children: ReactNode;
+  id: Id<"messages">;
 }) {
+  const deleteMessage = useMutation(api.messages.deleteMessage);
+
+  const handleClick = async () => {
+    try {
+      await deleteMessage({ id });
+    } catch (error) {
+      throw new Error("Can't delete");
+    }
+  };
+
+  const handleDoubleClick = () => {
+    handleClick()
+      .then(() => console.log("message deleted"))
+      .catch((error) => console.error(error));
+  };
+
   return (
     <li
       className={cn(
@@ -26,6 +47,7 @@ export function Message({
           "rounded-xl bg-muted px-3 py-2",
           author === viewer ? "rounded-tr-none" : "rounded-tl-none",
         )}
+        onDoubleClick={handleDoubleClick}
       >
         {children}
       </p>
