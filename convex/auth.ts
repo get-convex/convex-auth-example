ae2da1a7d0702277f64d29d6a8d3627b82bc02c8
Tom Ballinger <tom@convex.dev>
2025-01-09 12:25:43 -0800

    Published from https://github.com/get-convex/convex-auth

---
+++ convex/auth.ts
@@ -0,0 +1,67 @@
+import { INVALID_PASSWORD } from "./errors.js"
+import GitHub from "@auth/core/providers/github";
+import Google from "@auth/core/providers/google";
+import Resend from "@auth/core/providers/resend";
+import Apple from "@auth/core/providers/apple";
+import { Anonymous } from "@convex-dev/auth/providers/Anonymous";
+import { Password } from "@convex-dev/auth/providers/Password";
+import { ConvexError } from "convex/values";
+import { convexAuth } from "@convex-dev/auth/server";
+import { ResendOTP } from "./otp/ResendOTP";
+import { TwilioOTP } from "./otp/TwilioOTP";
+import { TwilioVerify } from "./otp/TwilioVerify";
+import { ResendOTPPasswordReset } from "./passwordReset/ResendOTPPasswordReset";
+import { DataModel } from "./_generated/dataModel.js";
+
+export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
+  providers: [
+    GitHub,
+    Google,
+    Apple({
+      clientSecret: process.env.AUTH_APPLE_SECRET!,
+      client: {
+        token_endpoint_auth_method: "client_secret_post",
+      },
+      profile: undefined,
+    }),
+    Resend({
+      from: process.env.AUTH_EMAIL ?? "My App <onboarding@resend.dev>",
+    }),
+    ResendOTP,
+    TwilioVerify,
+    TwilioOTP,
+    Password,
+    // Sample password auth with a custom parameter provided during sign-up
+    // flow and custom password validation requirements (at least six chars
+    // with at least one number, upper and lower case chars).
+    Password<DataModel>({
+      id: "password-custom",
+      profile(params, _ctx) {
+        return {
+          email: params.email as string,
+          favoriteColor: params.favoriteColor as string,
+        };
+      },
+      validatePasswordRequirements: (password: string) => {
+        if (
+          !password ||
+          password.length < 6 ||
+          !/\d/.test(password) ||
+          !/[a-z]/.test(password) ||
+          !/[A-Z]/.test(password)
+        ) {
+          throw new ConvexError(INVALID_PASSWORD);
+        }
+      },
+    }),
+    Password({ id: "password-with-reset", reset: ResendOTPPasswordReset }),
+    Password({
+      id: "password-code",
+      reset: ResendOTPPasswordReset,
+      verify: ResendOTP,
+    }),
+    // This one only makes sense with routing, ignore for now:
+    Password({ id: "password-link", verify: Resend }),
+    Anonymous,
+  ],
+});
---
+++ convex/users.ts
@@ -0,0 +1,10 @@
+import { query } from "./_generated/server";
+import { getAuthUserId } from "@convex-dev/auth/server";
+
+export const viewer = query({
+  args: {},
+  handler: async (ctx) => {
+    const userId = await getAuthUserId(ctx);
+    return userId !== null ? ctx.db.get(userId) : null;
+  },
+});
